93122e12def1767e3a1cf44f219a4e9449b65a89
Hesam-Eskandari <eskandarihesam@gmail.com>
2021-09-02 01:30:52 -0600

    add get menu ingredient for alpha

---
+++ pkg/api/alpha/menu_get_ingredients.go
@@ -0,0 +1,44 @@
+package alpha
+
+import (
+	"context"
+	"fmt"
+	"github.com/flypay/engineering-test/pkg/api/apiHandler"
+	"github.com/flypay/engineering-test/pkg/internal"
+	"github.com/flypay/engineering-test/pkg/service"
+	"net/http"
+)
+
+type getMenuIngredientsHandler struct {
+	clientBaseURL string
+}
+
+func NewGetMenuIngredients() apiHandler.Handler {
+	return &getMenuIngredientsHandler{
+		clientBaseURL: internal.AlphaClientBaseURL,
+	}
+}
+
+func (h *getMenuIngredientsHandler) URL() string {
+	return internal.BasePathAlpha + internal.MenuIngredientsAlpha
+}
+
+func (h *getMenuIngredientsHandler) Methods() []string {
+	return []string{http.MethodGet}
+}
+
+func (h *getMenuIngredientsHandler) ParseArgs(r *http.Request) (*http.Request, error) {
+	fmt.Println("Parsing Args")
+	ctx := context.WithValue(r.Context(), internal.MethodContextKey, http.MethodGet)
+	ctx = context.WithValue(ctx, internal.POSAddressContextKey,
+		fmt.Sprintf("http://%s"+"%s", h.clientBaseURL, internal.MenuIngredientsAlpha))
+	r = r.WithContext(ctx)
+	return r, nil
+}
+
+func (h *getMenuIngredientsHandler) Process(r *http.Request) *http.Response {
+	ctx := r.Context()
+	method := ctx.Value(internal.MethodContextKey).(string)
+	destination := ctx.Value(internal.POSAddressContextKey).(string)
+	return service.RequestClient(method, destination, nil)
+}
