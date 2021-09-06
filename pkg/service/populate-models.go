ef7b70b698ebe3372bb8da554a789a5444502e1b
Hesam-Eskandari <eskandarihesam@gmail.com>
2021-09-05 18:17:49 -0600

    add PopulateUnifiedOrderRespBody function

--- pkg/service/populate-models.go
+++ pkg/service/populate-models.go
@@ -1,186 +1,78 @@
 package service
 
 import (
-	"fmt"
-	"log"
 	"math"
-	"net/http"
-	"strconv"
 
 	"github.com/flypay/engineering-test/pkg/internal"
 	"github.com/flypay/engineering-test/pkg/internal/schema"
 )
 
-func populateUnifiedRespBodyForBeta(reqBody *schema.OrderRequest, unifiedBody *schema.OrderResponse,
-	menu *schema.BetaMenu) {
+func PopulateUnifiedOrderRespBody(reqBody *schema.OrderRequest, menu *schema.Menu, unifiedBody *schema.OrderResponse) {
 	unifiedBody.ID = reqBody.ID
 	unifiedBody.POS = reqBody.POS
 
-	type itemAddons struct {
-		Name  string  `json:"name"`
-		Price float32 `json:"price"`
-	}
-
-	newItemAddons := func(name string, price float32) *itemAddons {
-		return &itemAddons{
-			Name:  name,
-			Price: price,
-		}
-	}
-
-	type menuItem struct {
-		Name  string  `json:"name"`
-		Price float32 `json:"price"`
-		//Quantity 	int 				`json:"quantity"`
-		AllowedAddOns map[string]bool `json:"allowedAddOns"`
-	}
-
-	newMenuItem := func(name string, price float32, allowedAddOns map[string]bool) *menuItem {
-		return &menuItem{
-			Name:          name,
-			Price:         price,
-			AllowedAddOns: allowedAddOns,
-		}
-	}
-
-	menuItemMap := make(map[string]*menuItem)
-	addOnsMap := make(map[string]*itemAddons)
-	for _, cat := range menu.Categories {
-		for itemID, item := range cat.Items {
-			allowedAddOns := make(map[string]bool)
-			for _, addOn := range item.AddOns {
-				allowedAddOns[addOn.ID] = true
-				addOnsMap[addOn.ID] = newItemAddons(addOn.Name, addOn.Price)
+	itemMap := make(map[string]*schema.MenuItem)
+	sizeMap := make(map[string]map[string]*schema.MenuItemSize)
+	ingredientMap := make(map[string]map[string]*schema.MenuItemIngredient)
+	extraMap := make(map[string]map[string]*schema.MenuItemExtra)
+	for _, menuCat := range menu.Categories {
+		for _, subCat := range menuCat.Subcategories {
+			for _, item := range subCat.Items {
+				itemMap[item.ID] = item
+				sizeMap[item.ID] = make(map[string]*schema.MenuItemSize)
+				for _, size := range item.Sizes {
+					sizeMap[item.ID][size.ID] = size
+				}
+				ingredientMap[item.ID] = make(map[string]*schema.MenuItemIngredient)
+				for _, ingredient := range item.Ingredients {
+					ingredientMap[item.ID][ingredient.ID] = ingredient
+				}
+				extraMap[item.ID] = make(map[string]*schema.MenuItemExtra)
+				for _, extra := range item.Extras {
+					extraMap[item.ID][extra.ID] = extra
+				}
 			}
-			menuItemMap[itemID] = newMenuItem(item.Name, item.Price, allowedAddOns)
 		}
 	}
-	var orderResponseItems []schema.OrderResponseItem
+	items := make([]*schema.OrderResponseItem, 0, len(reqBody.Items))
 	var totalPrice float32
-	for _, item := range reqBody.Items {
-		price := menuItemMap[item.ID].Price * float32(item.Quantity)
-		extras := make([]string, 0)
-		for _, extra := range append(item.Extras, item.Ingredients...) {
-			if _, ok := menuItemMap[item.ID].AllowedAddOns[extra]; ok {
-				price += addOnsMap[extra].Price
-				extras = append(extras, addOnsMap[extra].Name)
-			}
-		}
-
-		orderResponseItems = append(orderResponseItems, schema.OrderResponseItem{
-			Name:     menuItemMap[item.ID].Name,
-			Quantity: item.Quantity,
-			Size:     "regular",
-			Extras:   extras,
-			Price:    float32(math.Round(float64(price*1000)) / 1000),
-		})
-		totalPrice += price
-	}
-	unifiedBody.Items = orderResponseItems
-	unifiedBody.TotalPrice = float32(math.Round(float64(totalPrice*1000)) / 1000)
-}
-
-func populateUnifiedRespBodyForAlpha(reqBody *schema.OrderRequest, unifiedBody *schema.OrderResponse) {
-	var err error
-
-	// get products menu from alpha pos
-	productsResp := RequestPOSClient(http.MethodGet,
-		fmt.Sprintf("http://%s"+"%s", internal.AlphaClientBaseURL, internal.MenuProductsAlpha), nil)
-	var products schema.AlphaProductsMenu
-	if err = DecodeReqRespBody(productsResp.Body, &products); err != nil {
-		log.Fatalf("failed decoding:, incompatible response body: %v", err)
-	}
-	// get ingredients menu from alpha pos
-	ingredientsResp := RequestPOSClient(http.MethodGet,
-		fmt.Sprintf("http://%s"+"%s", internal.AlphaClientBaseURL, internal.MenuIngredientsAlpha), nil)
-	var ingredients schema.AlphaIngredientsMenu
-	if err = DecodeReqRespBody(ingredientsResp.Body, &ingredients); err != nil {
-		log.Fatalf("failed decoding:, incompatible response body: %v", ingredientsResp.Body)
-	}
-	// ingredientItem represents a structure that includes ingredient name and price
-	type ingredientItem struct {
-		name  string
-		price float32
-	}
-
-	// newIngredientItem is a function literal to construct a new ingredientItem instance
-	newIngredientItem := func(name string, price float32) *ingredientItem {
-		return &ingredientItem{name, price}
-	}
-
-	// productSizeItem represents a structure that includes product size name and product price
-	type productSizeItem struct {
-		name  string
-		price float32
-	}
-
-	// productItem represents a structure that includes product name, sizes, and an array of allowed extras
-	type productItem struct {
-		name          string
-		sizes         map[string]*productSizeItem
-		allowedExtras map[string]bool
-	}
-
-	// newProductItem is a function literal to construct a new productItem instance
-	newProductItem := func(name string, sizesMap map[string]*productSizeItem, allowedExtras map[string]bool) *productItem {
-		return &productItem{
-			name:          name,
-			sizes:         sizesMap,
-			allowedExtras: allowedExtras,
-		}
-	}
-
-	// ingredientsMap maps ingredient ids to ingredientItem (includes ingredient name and price)
-	ingredientsMap := make(map[string]*ingredientItem)
-
-	// productsMap maps product ids to productItem (includes product name, sizes, and an array of allowed extras)
-	productsMap := make(map[string]*productItem)
-	for _, ingredient := range ingredients.Ingredients {
-		ingredientsMap[ingredient.IngredientID] = newIngredientItem(ingredient.Name, 0)
-	}
-	for _, product := range products.Products {
-		allowedExtras := make(map[string]bool)
-		for _, extra := range product.Extras {
-			allowedExtras[extra.IngredientID] = true
-			if _, ok := ingredientsMap[extra.IngredientID]; ok {
-				ingredientsMap[extra.IngredientID].price = extra.Price
-			} else {
-				log.Printf("unknown ingredient id: %v, pos: %v", extra.IngredientID, reqBody.POS)
-			}
-		}
-		sizesMap := make(map[string]*productSizeItem)
-		for _, size := range product.Sizes {
-			sizesMap[size.SizeID] = &productSizeItem{size.Name, size.Price}
-		}
-		productsMap[product.ProductID] = newProductItem(product.Name, sizesMap, allowedExtras)
-	}
-
-	// populate unifiedBody
-	var totalPrice float32
-	unifiedBody.ID = reqBody.ID
-	unifiedBody.POS = reqBody.POS
-	for _, item := range reqBody.Items {
+	for _, reqItem := range reqBody.Items {
 		var price float32
-		extras := make([]string, 0, len(item.Extras))
-		for _, extraID := range append(item.Ingredients, item.Extras...) {
-			fmt.Println("extra", extraID)
-			if _, ok := productsMap[item.ID].allowedExtras[extraID]; ok {
-				extras = append(extras, ingredientsMap[extraID].name)
-				price += ingredientsMap[extraID].price
+		item := new(schema.OrderResponseItem)
+		item.Quantity = reqItem.Quantity
+		item.Name = itemMap[reqItem.ID].Name
+		price += sizeMap[reqItem.ID][reqItem.Size].Price * float32(item.Quantity)
+		item.Size = sizeMap[reqItem.ID][reqItem.Size].Name
+		if len(reqItem.Ingredients) == 0 {
+			ingredients := make([]string, 0, len(ingredientMap[reqItem.ID]))
+			for _, menuIngredient := range itemMap[reqItem.ID].Ingredients {
+				ingredients = append(ingredients, menuIngredient.Name)
+			}
+			item.Ingredients = ingredients
+		} else {
+			ingredients := make([]string, 0, len(reqItem.Ingredients))
+			for _, ingredientID := range reqItem.Ingredients {
+				if _, ok := ingredientMap[reqItem.ID][ingredientID]; ok {
+					ingredients = append(ingredients, ingredientMap[reqItem.ID][ingredientID].Name)
+				}
+			}
+			item.Ingredients = ingredients
+		}
+
+		extras := make([]string, 0, len(reqItem.Extras))
+		for _, extraID := range reqItem.Extras {
+			if _, ok := extraMap[reqItem.ID][extraID]; ok {
+				extras = append(extras, extraMap[reqItem.ID][extraID].Name)
+				price += extraMap[reqItem.ID][extraID].Price
 			}
 		}
-		price += float32(item.Quantity) * productsMap[item.ID].sizes[item.Size].price
-		unifiedBody.Items = append(unifiedBody.Items, schema.OrderResponseItem{
-			Name:     productsMap[item.ID].name,
-			Quantity: item.Quantity,
-			Size:     productsMap[item.ID].sizes[item.Size].name,
-			Extras:   extras,
-			Price:    float32(math.Round(float64(price*1000)) / 1000),
-		})
+		item.Extras = extras
+		item.Price = float32(math.Round(float64(price*100)) / 100)
+		items = append(items, item)
 		totalPrice += price
 	}
-	unifiedBody.TotalPrice = float32(math.Round(float64(totalPrice*1000)) / 1000)
-	return
+	unifiedBody.TotalPrice = float32(math.Round(float64(totalPrice*100)) / 100)
+	unifiedBody.Items = items
 }
 
 func PopulateUnifiedMenuFromAlphaMenu(alphaMenu *schema.AlphaMenu, unifiedMenu *schema.Menu) {
@@ -277,14 +169,14 @@ func PopulateUnifiedMenuFromBetaMenu(betaMenu *schema.BetaMenu, unifiedMenu *sch
 			item := new(schema.MenuItem)
 			item.ID = itemID
 			item.Name = betaItem.Name
-
+			item.Description = betaItem.Description
 			sizes := make([]*schema.MenuItemSize, 0, 1)
 			item.Sizes = append(sizes, &schema.MenuItemSize{Name: "Regular", ID: itemID, Price: betaItem.Price})
 
 			extras := make([]*schema.MenuItemExtra, 0, len(betaItem.AddOns))
-			for addOnID, addOn := range betaItem.AddOns {
+			for _, addOn := range betaItem.AddOns {
 				extra := new(schema.MenuItemExtra)
-				extra.ID = strconv.Itoa(addOnID)
+				extra.ID = addOn.ID
 				extra.Name = addOn.Name
 				extra.Price = addOn.Price
 				extras = append(extras, extra)
