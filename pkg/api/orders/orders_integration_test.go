873858eb78afa09bffec58ae66afc3cea97fb505
Hesam-Eskandari <eskandarihesam@gmail.com>
2021-09-07 00:40:49 -0600

    modify integration test

--- pkg/api/orders/orders_integration_test.go
+++ pkg/api/orders/orders_integration_test.go
@@ -22,7 +22,7 @@ func TestAlphaOrderResponse(t *testing.T) {
 	// 	2x Regular Fries
 	//		Paprika Salt
 	//  2x Large Fizzy Cola
-	requestOrder := setUpAlphaOrder()
+	requestOrder := schema.NewUnifiedOrderRequestPopulatedOrderAlphaMock()
 	repo := repository.NewRepositoryImpl()
 	serv := service.NewServiceImpl()
 	requestPayload, _ := repo.EncodeReqRespBody(requestOrder)
@@ -33,13 +33,13 @@ func TestAlphaOrderResponse(t *testing.T) {
 	if err := repo.DecodeReqRespBody(alphaOrderResp.Body, alphaOrderResponse); err != nil {
 		t.Fatalf("failed decoding body resp. err: %s", err.Error())
 	}
-	expectedResponse := setUpAlphaResponse()
+	expectedResponse := schema.NewUnifiedRespPopulatedAlphaOrderRespMock()
 	assertEqualOrders(t, expectedResponse, alphaOrderResponse)
 }
 
 // TestBetaOrderResponse calls unified service url to set an order to beta pos
 func TestBetaOrderResponse(t *testing.T) {
-	requestOrder := setUpBetaOrder()
+	requestOrder := schema.NewUnifiedOrderRequestPopulatedOrderBetaMock()
 	repo := repository.NewRepositoryImpl()
 	serv := service.NewServiceImpl()
 	requestPayload, _ := repo.EncodeReqRespBody(requestOrder)
@@ -50,7 +50,7 @@ func TestBetaOrderResponse(t *testing.T) {
 	if err := repo.DecodeReqRespBody(betaOrderResp.Body, betaOrderResponse); err != nil {
 		t.Fatalf("failed decoding body resp. err: %s", err.Error())
 	}
-	expectedResponse := setUpBetaResponse()
+	expectedResponse := schema.NewUnifiedRespPopulatedBetaOrderRespMock()
 	assertEqualOrders(t, expectedResponse, betaOrderResponse)
 }
 
@@ -62,147 +62,4 @@ func assertEqualOrders(t *testing.T, expectedModel, retrievedModel *schema.Order
 	if !reflect.DeepEqual(expectedModel, retrievedModel) {
 		t.Fatal("The two struct are not equal")
 	}
-}
-
-// setUpAlphaOrder returns an alpha test order request body
-func setUpAlphaOrder() (order *schema.OrderRequest) {
-	order = new(schema.OrderRequest)
-	order.POS = internal.POSAlpha
-	order.ID = "12345"
-	order.Items = []schema.OrderItem{
-		{
-			ID:          "6001",
-			Quantity:    1,
-			Size:        "8001",
-			Ingredients: []string{"9004"},
-			Extras:      []string{"9010"},
-		},
-		{
-			ID:          "6002",
-			Quantity:    1,
-			Size:        "8001",
-			Ingredients: []string{},
-			Extras:      []string{},
-		},
-		{
-			ID:          "6003",
-			Quantity:    2,
-			Size:        "8005",
-			Ingredients: []string{},
-			Extras:      []string{"9009"},
-		},
-		{
-			ID:          "6004",
-			Quantity:    2,
-			Size:        "8006",
-			Ingredients: []string{},
-			Extras:      []string{},
-		},
-	}
-	return
-}
-
-// setUpAlphaResponse returns expected alpha order response that should be returned
-func setUpAlphaResponse() (resp *schema.OrderResponse) {
-	resp = new(schema.OrderResponse)
-	resp.POS = internal.POSAlpha
-	resp.ID = "12345"
-	resp.TotalPrice = 48.68
-	resp.Items = []*schema.OrderResponseItem{
-		{
-			Name:        "Mushroom Burger",
-			Quantity:    1,
-			Size:        "Individual",
-			Extras:      []string{"Extra Mayo"},
-			Ingredients: []string{"Seeded Bun", "Mushroom Patty", "Lettuce"},
-			Price:       15.94,
-		},
-		{
-			Name:        "Buttermilk Chicken Burger",
-			Quantity:    1,
-			Size:        "Individual",
-			Extras:      []string{},
-			Ingredients: []string{"Brioche Bun", "Chicken Patty", "Pickles", "Slaw"},
-			Price:       15.95,
-		},
-		{
-			Name:        "Fries",
-			Quantity:    2,
-			Size:        "Regular",
-			Extras:      []string{"Paprika Salt"},
-			Ingredients: []string{},
-			Price:       8.89,
-		},
-		{
-			Name:        "Fizzy Cola",
-			Quantity:    2,
-			Size:        "Large",
-			Extras:      []string{},
-			Ingredients: []string{},
-			Price:       7.9,
-		},
-	}
-	return
-}
-
-// setUpBetaOrder returns an beta test order request body
-func setUpBetaOrder() (order *schema.OrderRequest) {
-	order = new(schema.OrderRequest)
-	order.POS = internal.POSBeta
-	order.ID = "54321"
-	order.Items = []schema.OrderItem{
-		{
-			ID:          "hjrlho",
-			Quantity:    1,
-			Size:        "hjrlho",
-			Ingredients: []string{},
-			Extras:      []string{"cbnufj"},
-		},
-		{
-			ID:       "ukhqnd",
-			Quantity: 2,
-			Size:     "ukhqnd",
-		},
-		{
-			ID:       "ugqcbb",
-			Quantity: 1,
-			Size:     "ugqcbb",
-		},
-	}
-	return
-}
-
-// setUpBetaResponse returns expected beta order response that should be returned
-func setUpBetaResponse() (resp *schema.OrderResponse) {
-	resp = new(schema.OrderResponse)
-	resp.ID = "54321"
-	resp.POS = internal.POSBeta
-	resp.TotalPrice = 17.79
-	resp.Items = []*schema.OrderResponseItem{
-		{
-			Name:        "Falafel Wrap",
-			Quantity:    1,
-			Size:        "Regular",
-			Extras:      []string{"Extra Chilli Sauce"},
-			Ingredients: []string{},
-			Price:       11.09,
-		},
-		{
-			Name:        "Jam Donut",
-			Quantity:    2,
-			Size:        "Regular",
-			Extras:      []string{},
-			Ingredients: []string{},
-			Price:       2.7,
-		},
-		{
-			Name:        "Bottled Water",
-			Quantity:    1,
-			Size:        "Regular",
-			Extras:      []string{},
-			Ingredients: []string{},
-			Price:       4,
-		},
-	}
-	return
 }
